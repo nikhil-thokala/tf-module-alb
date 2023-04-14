3d30096d33522107157395e48fc99e26b49ec3b6
nikhilt <nikhil.t1226@gmail.com>
2023-04-14 20:26:54 +0100

    \ |"?

--- vars.tf
+++ vars.tf
@@ -3,7 +3,10 @@ variable "tags" {}
 variable "name" {}
 variable "internal" {}
 variable "load_balancer_type" {}
+
+
 variable "enable_deletion_protection" {
   default = false
 }
-variable = "subnets" {}
+
+variable "subnets" {}
